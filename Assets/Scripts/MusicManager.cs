3a5983b85007ff122dfbc8285ef54f5ad2473074
Rizki Febrian <541231191@student.smktelkom-pwt.sch.id>
2025-06-06 16:44:58 +0700

    revisimusic

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,19 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     public static MusicManager instance;
+    private AudioSource audioSource;
+    private string lastSceneName = "";
 
     void Awake()
     {
         if (instance != null)
+        {
             Destroy(gameObject);
+        }
         else
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    void Update()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        // Jika kembali ke Home dan bukan scene sebelumnya, restart musik
+        if (currentScene == "MainMenu" && currentScene != lastSceneName)
+        {
+            RestartMusic();
+        }
+
+        lastSceneName = currentScene;
+    }
+
+    void RestartMusic()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Stop();
+            audioSource.Play();
         }
     }
 }
